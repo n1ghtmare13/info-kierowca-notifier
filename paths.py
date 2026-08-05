6d40e64e76b29afc28d38b8a2d807f0d0d2e5080
n1ghtmare13 <mateusz.plecha@tm1.edu.pl>
2026-08-05 14:33:30 +0200

    Bump version to 1.4.1

--- paths.py
+++ paths.py
@@ -13,7 +13,7 @@ can sit at the bottom of the import graph and be safely imported everywhere.
 """
 from pathlib import Path
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 CONFIG_DIR = Path.home() / ".config" / "info-kierowca-notifier"
 CONFIG_FILE = CONFIG_DIR / "config.json"
